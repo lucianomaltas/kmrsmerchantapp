55e39f6eb80c5d1310e20f62bd843f331390cdf4
lucianomaltas <comercial@juxta.com.br>
2020-01-04 09:19:37 -0300

    Initial commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={					
+	'ApiUrl':"http://pizzasdogaucho.com.br/merchantapp/api",
+	'DialogDefaultTitle':"PED",
+	'APIHasKey':"gt785477910fa4a10f3124525bc6c69524f",
+	'debug': false
+};
